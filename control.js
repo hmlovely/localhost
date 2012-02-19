113e302f12202a9384e77cf4cffe4321bbb7a627
xiongsongsong <songsong@JeffmatoMacBook-Pro.local>
2012-02-19 12:51:31 +0800

    优化MimeType的加载方式

--- control.js
+++ control.js
@@ -10,77 +10,49 @@
 var route = require('./route'),
     fs = require('fs'),
     // jade = require('jade'),
-    path = require('path');
+    path = require('path'),
+    mime = require('./mime');
 
 exports.init = function (req, res) {
     var root = route.maps[req.headers.host];
-    root = root + decodeURIComponent(req.url.replace(/\//gi, '/'));
-    console.log('req.url:' + req.url);
+    root += decodeURIComponent(req.url.replace(/\//gi, '/'));
     var extname = path.extname(root);
-
-    console.log('extname' + extname);
+    extname = extname !== '' ? extname.substring(1, extname.length) : '';
     if (path.existsSync(root)) {
-        fs.readdir(root, function (err, files) {
-            if (!err) {
-                res.writeHead(200, {'Content-type':'text/html; charset="utf-8"'});
-                res.write('<ul>');
-                files.forEach(function (item) {
-                    res.write('<li><a href="' + (req.url === '/' ? '' : req.url) + '/' + item + '">' + item + '</a></li>');
+        fs.stat(root, function (err, stats) {
+            //如果是目录，则读取目录的所有文件
+            if (stats.isDirectory()) {
+                fs.readdir(root, function (err, files) {
+                    res.writeHead(200, {'Content-type':'text/html; charset="utf-8"'});
+                    res.write('<ul>');
+                    files.forEach(function (item) {
+                        res.write('<li><a href="' + (req.url === '/' ? '' : req.url) + '/' + item + '">' + item + '</a></li>');
+                    })
+                    res.write('</ul>');
+                    res.end();
                 })
-                res.write('</ul>');
-                res.end();
-            } else {
-                fs.readFile(root, function (err, data) {
 
+            }
+            //if是文件则读取文件内容
+            if (stats.isFile()) {
+                fs.readFile(root, function (err, data) {
                     if (!err) {
-                        switch (extname.substring(1, extname.length)) {
-                            case "png":
-                                res.writeHead(200, {'Content-type':'image/png;"'});
-                                break;
-                            case "jpg":
-                                res.writeHead(200, {'Content-type':'image/jpeg;"'});
-                                break;
-                            case "html":
-                                res.writeHead(200, {'Content-type':'text/html; charset="utf-8"'});
-                                break;
-                            case "js":
-                                res.writeHead(200, {'Content-type':'text/javascript; charset="utf-8"'});
-                                break;
-                            case "css":
-                                res.writeHead(200, {'Content-type':'text/css; charset="utf-8"'});
-                                break;
-                            case "xml":
-                                res.writeHead(200, {'Content-type':'text/xml; charset="utf-8"'});
-                                break;
-                            case "gif":
-                                res.writeHead(200, {'Content-type':'image/gif;'});
-                                break;
-                            case "txt":
-                                res.writeHead(200, {'Content-type':'text/plain; charset="utf-8"'});
-                                break;
-                            case "htm":
-                                res.writeHead(200, {'Content-type':'text/html; charset="utf-8"'});
-                                break;
-                            default:
-                                res.writeHead(200, {'Content-type':'object/stram; charset="utf-8"'});
-                                break;
-                        }
-                        console.log('It\'s saved!');
+                        console.log(mime.maps[extname] ? mime.maps[extname] : 'object/stream')
+                        res.writeHead(200, {'Content-Type':mime.maps[extname] ? mime.maps[extname] : 'object/stream'})
                         res.end(data);
                     } else {
-                        res.end('not exist!');
-
+                        res.end(err.toString());
                     }
                 });
             }
-            /*var obj = {
-             'fileList':files
-             };
-             obj['pageTitle'] = root;
-             var fn = jade.compile(fs.readFileSync('./views/main-content.jade'));*/
 
-        });
-    } else {
-        res.end(root + 'not exist!');
+        })
     }
 };
+
+
+/*var obj = {
+ 'fileList':files
+ };
+ obj['pageTitle'] = root;
+ var fn = jade.compile(fs.readFileSync('./views/main-content.jade'));*/
---
+++ mime.js
@@ -0,0 +1,21 @@
+/**
+ * Created by JetBrains WebStorm.
+ * User: songsong
+ * Date: 12-2-19
+ * Time: 下午12:25
+ * To change this template use File | Settings | File Templates.
+ */
+exports.maps = {
+    'png':'image/png;',
+    'gif':'image/gif',
+    'jpg':'image/jpeg',
+    'jpeg':'image/jpeg',
+    'bmp':'image/bmp',
+    'json':'text/javascript',
+    'js':'text/javascript',
+    'html':'text/html',
+    'htm':'text/html',
+    'css':'text/css',
+    'xml':'text/xml',
+    'txt':'text/plain'
+};

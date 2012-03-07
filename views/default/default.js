5ca4c5ef49e409f975117c53b3188377fa686886
unknown <songsong@song.(none)>
2012-03-07 11:30:23 +0800

    you can prview the image

--- views/default/default.js
+++ views/default/default.js
@@ -7,3 +7,14 @@
  */
 
 document.title += '--HELLO';
+$(document.body).append('<p id="prview"><img src=""></p>');
+$prviewObj = $('#prview');
+$('li').hover(function () {
+    var _src = $(this).find('a').eq(0).attr('href');
+    if (/(png|jpg|jpeg|bmp|gif|ico)/i.test(_src)) {
+        $prviewObj.show();
+        $prviewObj.find('img').eq(0).attr('src', _src);
+    }
+}, function () {
+    $prviewObj.hide();
+});

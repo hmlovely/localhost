da6e29704d46b416d165bc06b7990e55390ef49d
何敏 <wb-hemin@taobao.com>
2012-03-20 15:46:12 +0800

    对样式稍作了修改

--- config.js
+++ config.js
@@ -1,43 +1,46 @@
-/**
- * Created by JetBrains PhpStorm.
- * User: hemin
- * Date: 12-2-18
- * Time: 13:42
- * To change this template use File | Settings | File Templates.
- */
+exports.config={};
 
-"use strict";
-
-exports.config = {
-    'localhost':{},
-    'local':{},
-    'currentView':'default',
-    'views':{
-        "default":{
-            'title':'默认主题',
-            'cssLink':['default/default', 'default/default2'],
-            'jsLink':['default/default']
-        },
-        'green':{
-            'title':'绿色心情',
-            'cssLink':['green/green'],
-            'jsLink':['green/green']
-        }
-    }
+exports.config.localhost ={
+	"path": "D:\\"
 };
 
+exports.config.local ={
+	"path": "D:\\localhost\\views"
+};
 
-var os = require('os'),
-    isWin = /(Windows)/i.test(os.type()),
-    isUnix = /(Darwin|Linux)/i.test(os.type());
+exports.config.yuanying ={
+	"path": "C:\\"
+};
 
-if (isUnix) {
-    exports.config.localhost.path = '/Users';
-    exports.config['local'].path = __dirname + '/views';
-} else if (isWin) {
-    exports.config.localhost.path = __dirname.substring(0, 2);
-    exports.config['local'].path = __dirname + '\\views';
+exports.config.currentView ="green";
 
-} else {
-    console.log('without the support of the operation system');
-}
+exports.config.views ={
+	"default": {
+		"title": "默认主题",
+		"cssLink": [
+			"default/default",
+			"default/default2"
+		],
+		"jsLink": [
+			"default/default"
+		]
+	},
+	"green": {
+		"title": "绿色心情",
+		"cssLink": [
+			"green/green"
+		],
+		"jsLink": [
+			"green/green"
+		]
+	},
+	"pink": {
+		"title": "粉色",
+		"cssLink": [
+			"pink/pink"
+		],
+		"jsLink": [
+			"pink/pink"
+		]
+	}
+};

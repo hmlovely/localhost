6c6e993cdad239cd3bcc9300aefcd0e5def07914
何敏 <wb-hemin@taobao.com>
2012-04-23 16:28:52 +0800

    添加了一个回到顶部的按钮

--- views/green/green.js
+++ views/green/green.js
@@ -6,4 +6,19 @@
  * To change this template use File | Settings | File Templates.
  */
 
-$('body').append('<p>我来自green.js</p>');
+//$('body').append('<p>我来自green.js</p>');
+
+document.title += '--HELLO';
+$(document.body).append('<p id="prview"><img src=""></p>');
+$prviewObj = $('#prview');
+$('li').hover(function () {
+    var href = $(this).find('a').eq(0).attr('href'),
+        _href = href;
+    _href = _href.substring(_href.lastIndexOf('.') + 1);
+    if (/(png|jpg|jpeg|bmp|gif|ico)$/gi.test(_href) === true) {
+        $prviewObj.show();
+        $prviewObj.find('img').eq(0).attr('src', href);
+    }
+}, function () {
+    $prviewObj.hide();
+});
